75e17fd22f071c64232862013ebb4993c79d553c
Phuong <phuongmaipham@icloud.com>
2016-06-30 14:35:42 +0000

    add video

--- player.c
+++ player.c
@@ -13,5 +13,7 @@ int main()
         printf("Date: Mon, 13 Jun 2016 15:48:49 GMT\n");
         printf("\n");
 
-        system("/opt/vc/bin/raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25|cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264");
+        system("/opt/vc/bin/raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25|cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264&");
+
+return 1;
 }
